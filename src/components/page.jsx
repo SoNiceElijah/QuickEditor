aaf621fa96e558bdde1ce8cc4f2e129524633964
Elijah <Superil2q@yandex.ru>
2020-08-28 18:05:43 +0300

    start pack

---
+++ src/components/page.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+export class Page extends React.Component {
+
+    render() {
+        return (
+            <div className="the-most-big-one">
+                <h1> Header </h1>
+            </div> 
+        );
+    }
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import { Page } from './components/page';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Page />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
